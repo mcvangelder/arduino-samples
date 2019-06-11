d3a2e3f15c95f6e1aab0da52f9601315ce8cdf50
Matthew Van Gelder <vangelder@gmail.com>
2019-06-11 14:09:56 -0700

    Simplified code to a single switch

--- led-state-machine/led-state-machine.ino
+++ led-state-machine/led-state-machine.ino
@@ -1,14 +1,14 @@
 #include "statemachine.h"
 
 StateMachine machine;
-StateData state1 = StateData(1);
 
 void setup()
 {
     pinMode(LED_BUILTIN, OUTPUT);
     digitalWrite(LED_BUILTIN, HIGH);
 
-    StateData state2 = StateData(2);
+    StateData state1 = StateData(0b01);
+    StateData state2 = StateData(0b10);
 
     StateData *state1Transitions[] = {&state2};
     StateData *state2Transitions[] = {&state1};
@@ -27,26 +27,13 @@ void setup()
 
 long transitionDelay = 1000;
 long lastTransition = 0;
+int nextValue = 2;
 
 void loop()
 {
     if (lastTransition == 0 || lastTransition + transitionDelay < millis())
     {
         lastTransition = millis();
-
-        int currentValue = machine.getCurrentStateValue();
-        int nextValue = 0;
-
-        switch (currentValue)
-        {
-        case 1:
-            nextValue = 2;
-            break;
-        case 2:
-            nextValue = 1;
-            break;
-        }
-
         machine.transitionTo(nextValue);
     }
 }
@@ -58,9 +45,11 @@ void onTransition(StateData *oldState, StateData *newState)
     switch (currentValue)
     {
     case 1:
+        nextValue = 2;
         digitalWrite(LED_BUILTIN, LOW);
         break;
     case 2:
+        nextValue = 1;
         digitalWrite(LED_BUILTIN, HIGH);
         break;
     default:
