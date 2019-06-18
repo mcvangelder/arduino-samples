5d78de7195a8bf6d8fccf05226cb16e1e106f40a
Matthew Van Gelder <vangelder@gmail.com>
2019-06-17 19:19:06 -0700

    Improved button inspection logic - removed switch statement Added Led state struct, Button state struct Added "Off" state to statemachine Enabled statemachine to be transitioned to "Off" state with button press/release

--- led-state-machine/led-state-machine.ino
+++ led-state-machine/led-state-machine.ino
@@ -2,57 +2,159 @@
 
 StateMachine machine;
 
+#define SYSTEM_OFF 0b00
+#define LED_ON 0b01
+#define LED_OFF 0b10
+
+struct LED
+{
+    int pin;
+    int output;
+    long lastStateChange;
+};
+
+struct BUTTON
+{
+    int pin;
+    long lastReadMillis;
+    bool isToggled;
+    char lastRead = LOW;
+    char currentState = LOW;
+    long debounceDelay = 50;
+};
+
+LED led;
+BUTTON button;
+
 void setup()
 {
-    pinMode(LED_BUILTIN, OUTPUT);
-    digitalWrite(LED_BUILTIN, HIGH);
+    Serial.begin(9600);
 
-    StateData state1 = StateData(0b01);
-    StateData state2 = StateData(0b10);
+    setupLED();
+    setUpButton();
+    setUpStateMachine();
 
-    StateData *state1Transitions[] = {&state2};
-    StateData *state2Transitions[] = {&state1};
-
-    state1.setAllowedTransitions(state1Transitions, 1);
-    state2.setAllowedTransitions(state2Transitions, 1);
-
-    StateData *allStates[] = {&state1, &state2};
-    machine = StateMachine(allStates, arrayLength(allStates), state1);
-    machine.setOnTransitionCallback(onTransition);
     delay(1000);
     pinMode(LED_BUILTIN, LOW);
     delay(500);
     pinMode(LED_BUILTIN, HIGH);
+    Serial.println("set up complete");
 }
 
 long transitionDelay = 1000;
-long lastTransition = 0;
-int nextValue = 2;
+int nextValue;
+int previousValue;
+bool isSystemOn;
 
 void loop()
 {
-    if (lastTransition == 0 || lastTransition + transitionDelay < millis())
+    inspectButton(&button);
+
+    if (previousValue != nextValue)
     {
-        lastTransition = millis();
         machine.transitionTo(nextValue);
+    }
+
+    if (isSystemOn)
+    {
+        if (led.lastStateChange + transitionDelay < millis())
+        {
+            execute(led);
+        }
+    }
+    else
+    {
+        off(led);
     }
 }
 
-void onTransition(StateData *oldState, StateData *newState)
+void setUpStateMachine()
 {
-    auto currentValue = newState->getValue();
+    Serial.println("Setting up machine");
+    StateData systemOffState = StateData(SYSTEM_OFF);
+    StateData ledOnState = StateData(LED_ON);
+    StateData ledOffState = StateData(LED_OFF);
 
-    switch (currentValue)
+    StateData *offStateTransitions[] = {&ledOnState};
+    systemOffState.setAllowedTransitions(offStateTransitions, 1);
+
+    StateData *ledOnStateTransitions[] = {&ledOffState, &systemOffState};
+    ledOnState.setAllowedTransitions(ledOnStateTransitions, 2);
+
+    StateData *ledStateOffTransitions[] = {&ledOnState, &systemOffState};
+    ledOffState.setAllowedTransitions(ledStateOffTransitions, 2);
+
+    StateData *states[] = {&systemOffState, &ledOnState, &ledOffState};
+
+    nextValue = SYSTEM_OFF;
+    previousValue = LED_OFF;
+    isSystemOn = false;
+    
+    machine = StateMachine(states, 3, ledOffState);
+    machine.setOnTransitionCallback(onStateChange);
+}
+
+void onStateChange(StateData *oldState, StateData *newState)
+{
+    auto newValue = newState->getValue();
+    previousValue = oldState->getValue();
+    isSystemOn = newValue != SYSTEM_OFF;
+    led.output = LOW;
+    if (isSystemOn)
     {
-    case 1:
-        nextValue = 2;
-        digitalWrite(LED_BUILTIN, LOW);
-        break;
-    case 2:
-        nextValue = 1;
-        digitalWrite(LED_BUILTIN, HIGH);
-        break;
-    default:
-        break;
+        led.output = newValue == LED_OFF ? LOW : HIGH;
+        led.lastStateChange = millis();
+    }
+}
+
+void execute(LED led)
+{
+    digitalWrite(led.pin, led.output);
+    nextValue = led.output == LOW ? LED_ON : LED_OFF;
+}
+
+void off(LED led)
+{
+    digitalWrite(led.pin, LOW);
+}
+
+void setupLED()
+{
+    led.pin = LED_BUILTIN;
+    led.output = LOW;
+    pinMode(led.pin, OUTPUT);
+    digitalWrite(led.pin, led.output);
+}
+
+void setUpButton()
+{
+    button.pin = 4;
+    button.isToggled = false;
+    pinMode(button.pin, INPUT);
+}
+
+void inspectButton(BUTTON *button)
+{
+    auto read = digitalRead(button->pin);
+    if (read != button->lastRead)
+    {
+        button->lastReadMillis = millis();
+        button->lastRead = read;
+    }
+
+    if (button->lastReadMillis + button->debounceDelay < millis())
+    {
+        if (button->currentState != read)
+        {
+            button->currentState = read;
+            // When the button is HIGH (pressed) preserve the previous value, when the
+            // the button is LOW (released) then flip the toggle
+            button->isToggled = read == LOW ? !button->isToggled : button->isToggled;
+            if (read == LOW)
+            {
+                auto currentState = machine.getCurrentStateValue();
+                nextValue = currentState == SYSTEM_OFF ? LED_ON : SYSTEM_OFF;
+            }
+        }
     }
 }
